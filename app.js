256692a4273f757cc63726644980752d2bcc8005
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-05-07 16:41:16 +0300

    App structure was created

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const express = require("express");
+var app = express();
+
+const bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = require("./helpers/db")();
+
+// Routes
+const indexRoute = require('./routes/index');
+app.use('/', indexRoute);
+
+const errorController = require('./controllers/errors');
+app.use(errorController.get404Page);
+
+
+
+app.listen(3000, () => {
+    console.log("App Started On Port : 3000");
+});
